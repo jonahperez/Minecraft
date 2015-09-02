e94bb184cf86f16f6e6324c5cc17967361f3492e
Jonah Perez <perez.jonah@gmail.com>
2015-09-02 14:19:33 -0700

    git fail

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 export AWS_DEFAULT_PROFILE=home
 aws s3 sync portal s3://www.jonahperez.com --region us-west-2
-
+aws cloudfront create-invalidation --distribution-id E2M80DV600H3AJ --invalidation-batch file://invalidation.json
