8bf1f09d3544990a8589c8999fe6ed9b290f20a1
Jonah Perez <perez.jonah@gmail.com>
2015-09-28 15:58:12 -0700

    changed backup to every hour

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,6 @@ service minecraft start
 aws s3 cp s3://jonahperez.com.minecraft/usr_bin/mon-put-instance-data.pl /usr/bin --region us-west-2
 chmod a+x /usr/bin/mon-put-instance-data.pl
 echo "*/5 * * * * mon-put-instance-data.pl --mem-util --mem-used --mem-avail --disk-space-used --disk-space-avail --disk-space-util --disk-path=/ --from-cron" >> /etc/crontab
-echo "0 0 * * * /etc/init.d/minecraft backup" >> /etc/crontab
+echo "0 * * * * /etc/init.d/minecraft backup" >> /etc/crontab
 crontab /etc/crontab
 
